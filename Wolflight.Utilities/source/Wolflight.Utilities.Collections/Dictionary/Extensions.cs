f139530b38dc7408c346bfb3989c7587313e11c0
brenthollett <brent.hollett@gmail.com>
2024-11-30 07:40:47 -0600

    Adjusted null protections for enumerable extensions.

--- Wolflight.Utilities/source/Wolflight.Utilities.Collections/Dictionary/Extensions.cs
+++ Wolflight.Utilities/source/Wolflight.Utilities.Collections/Dictionary/Extensions.cs
@@ -13,8 +13,23 @@
         /// <param name="dictionary">The dictionary to add a value to.</param>
         /// <param name="key">The key of the set.</param>
         /// <param name="value">The value to add.</param>
-        public static void AddSetValue<TKey, TValue>(this IDictionary<TKey, ISet<TValue>> dictionary, TKey key, TValue value)
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="dictionary"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="key"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="value"/> was <see langword="null"/>.
+        /// </exception>
+        public static void AddSetValue<TKey, TValue>(
+            this IDictionary<TKey, ISet<TValue>> dictionary,
+            TKey key,
+            TValue value
+        )
         {
+            ArgumentNullException.ThrowIfNull(value);
+
             GetValues(dictionary, key).Add(value);
         }
 
@@ -26,13 +41,29 @@
         /// <param name="dictionary">The dictionary to add values to.</param>
         /// <param name="key">The key of the set.</param>
         /// <param name="values">The values to add.</param>
-        public static void AddSetValues<TKey, TValue>(this IDictionary<TKey, ISet<TValue>> dictionary, TKey key, IEnumerable<TValue> values)
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="dictionary"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="key"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="values"/> was <see langword="null"/>.
+        /// </exception>
+        public static void AddSetValues<TKey, TValue>(
+            this IDictionary<TKey, ISet<TValue>> dictionary,
+            TKey key,
+            IEnumerable<TValue> values
+        )
         {
+            ArgumentNullException.ThrowIfNull(values);
+
             GetValues(dictionary, key).UnionWith(values);
         }
 
         /// <summary>
-        /// Removes a value to an <see cref="ISet{TValue}"/> stored against a key, and returns whether the item was in the set.
+        /// Removes a value to an <see cref="ISet{TValue}"/> stored 
+        /// against a key, and returns whether the item was in the set.
         /// </summary>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TValue">The type of the values in the set.</typeparam>
@@ -40,13 +71,28 @@
         /// <param name="key">The key of the set.</param>
         /// <param name="value">The value to remove.</param>
         /// <returns>
-        ///	 True if the value was removed from the set, otherwise false if the value was not found or the key did not exist.
+        ///	 True if the value was removed from the set, otherwise False
+        ///	 if the value was not found or the key did not exist.
         /// </returns>
         /// <remarks>If the last item from a set is removed, the set will also be removed.</remarks>
-        /// <exception cref="ArgumentNullException">The <paramref name="dictionary"/> was <see langword="null"/>.</exception>
-        public static bool RemoveSetValue<TKey, TValue>(this IDictionary<TKey, ISet<TValue>> dictionary, TKey key, TValue value)
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="dictionary"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="key"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="value"/> was <see langword="null"/>.
+        /// </exception>
+        public static bool RemoveSetValue<TKey, TValue>(
+            this IDictionary<TKey, ISet<TValue>> dictionary,
+            TKey key,
+            TValue value
+        )
         {
             ArgumentNullException.ThrowIfNull(dictionary);
+            ArgumentNullException.ThrowIfNull(key);
+            ArgumentNullException.ThrowIfNull(value);
 
             bool rc = false;
 
@@ -71,11 +117,23 @@
         /// <summary>
         /// <typeparam name="TKey">The type of the key.</typeparam>
         /// <typeparam name="TValue">The type of the values in the set.</typeparam>
-        /// <param name="dictionary">The dictionary to remove a value from.</param>
+        /// <param name="dictionary">The dictionary to get the values from.</param>
         /// <param name="key">The key of the set.</param>
         /// <returns>The set stored against the key.
-        public static ISet<TValue> GetValues<TKey, TValue>(this IDictionary<TKey, ISet<TValue>> dictionary, TKey key)
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="dictionary"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="key"/> was <see langword="null""/>.
+        /// </exception>
+        public static ISet<TValue> GetValues<TKey, TValue>(
+            this IDictionary<TKey, ISet<TValue>> dictionary,
+            TKey key
+        )
         {
+            ArgumentNullException.ThrowIfNull(dictionary);
+            ArgumentNullException.ThrowIfNull(key);
+
             if (!dictionary.TryGetValue(key, out ISet<TValue>? items))
             {
                 items = new HashSet<TValue>();
@@ -85,8 +143,31 @@
             return items;
         }
 
-        public static bool HasValue<TKey, TValue>(this IDictionary<TKey, ISet<TValue>> dictionary, TKey key, TValue value)
+        /// <summary>
+        /// Returns True if a <paramref name="key"/> exists in the <paramref name="dictionary"/>
+        /// containing the given <paramref name="value"/>, otherwise False.
+        /// </summary>
+        /// <typeparam name="TKey">The type of the key.</typeparam>
+        /// <typeparam name="TValue">The type of the values in the set.</typeparam>
+        /// <param name="dictionary">The dictionary to search for a value from.</param>
+        /// <param name="key">The key of the set to check.</param>
+        /// <param name="value">The value to search for</param>
+        /// <returns></returns>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="dictionary"/> was <see langword="null""/>.
+        /// </exception>
+        /// <exception cref="ArgumentNullException">
+        /// The <paramref name="key"/> was <see langword="null""/>.
+        /// </exception>
+        public static bool HasValue<TKey, TValue>(
+            this IDictionary<TKey, ISet<TValue>> dictionary,
+            TKey key,
+            TValue value
+        )
         {
+            ArgumentNullException.ThrowIfNull(dictionary);
+            ArgumentNullException.ThrowIfNull(key);
+
             if (dictionary.TryGetValue(key, out ISet<TValue>? items))
             {
                 return (items != null) && (items.Contains(value));
