ea3e72c7bfffc40ca5330ff02bc8d8d392a8b392
perringaiden <brent.hollett@gmail.com>
2025-02-09 13:33:40 +1000

    Added ArgumentZeroException.

---
+++ Wolflight.Utilities/source/Wolflight.Utilities.Exceptions/ArgumentZeroException.cs
@@ -0,0 +1,149 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Wolflight.Utilities.Exceptions
+{
+    public class ArgumentZeroException : ArgumentException
+    {
+
+        private const string ArgumentZeroString = "The provided argument was zero.";
+        private const string ArgumentZeroOrLessString = "The provided argument was zero or less.";
+
+        /// <summary>
+        /// Creates a new <see cref="ArgumentZeroException"/>.
+        /// </summary>
+        /// <param name="message">The message describing the </param>
+        private ArgumentZeroException(string? message, string? paramName) : base(message, paramName) { }
+
+        #region Integers
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        /// <exception cref="ArgumentZeroException">The argument was zero.</exception>
+        public static void ThrowIfZero(
+            int argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument == 0)
+            {
+                ThrowZeroException(paramName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero or less.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        public static void ThrowIfZeroOrLess(
+            int argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument <= 0)
+            {
+                ThrowZeroOrLessException(paramName);
+            }
+        }
+
+        #endregion
+
+        #region Longs
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        /// <exception cref="ArgumentZeroException">The argument was zero.</exception>
+        public static void ThrowIfZero(
+            long argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument == 0L)
+            {
+                ThrowZeroException(paramName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero or less.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        public static void ThrowIfZeroOrLess(
+            long argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument <= 0L)
+            {
+                ThrowZeroOrLessException(paramName);
+            }
+        }
+
+        #endregion
+
+        #region Decimal
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        /// <exception cref="ArgumentZeroException">The argument was zero.</exception>
+        public static void ThrowIfZero(
+            decimal argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument == 0M)
+            {
+                ThrowZeroException(paramName);
+            }
+        }
+
+        /// <summary>
+        /// Throws an exception if the provided <paramref name="argument"/> is zero or less.
+        /// </summary>
+        /// <param name="argument">The argument to evaluate.</param>
+        /// <param name="paramName">The parameter name of the argument.</param>
+        public static void ThrowIfZeroOrLess(
+            decimal argument,
+            [CallerArgumentExpression(nameof(argument))] string? paramName = null)
+        {
+            if (argument <= 0M)
+            {
+                ThrowZeroOrLessException(paramName);
+            }
+        }
+
+        #endregion
+
+        #region Exceptions
+
+        /// <summary>
+        /// Throws an exception for a zero parameter.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter.</param>
+        /// <exception cref="ArgumentZeroException">The argument was zero.</exception>
+        private static void ThrowZeroException(string? paramName)
+        {
+            ArgumentNullException.ThrowIfNull(paramName);
+            throw new ArgumentZeroException(ArgumentZeroString, paramName);
+        }
+
+        /// <summary>
+        /// Throws an exception for a zero or less parameter.
+        /// </summary>
+        /// <param name="paramName">The name of the parameter.</param>
+        /// <exception cref="ArgumentZeroException">The argument was zero or less.</exception>
+        private static void ThrowZeroOrLessException(string? paramName)
+        {
+            ArgumentNullException.ThrowIfNull(paramName);
+            throw new ArgumentZeroException(ArgumentZeroOrLessString, paramName);
+        }
+
+        #endregion
+
+    }
+}
---
+++ Wolflight.Utilities/tests/Wolflight.Utilities.Exceptions.UnitTests/ArgumentZeroExceptionTests.cs
@@ -0,0 +1,124 @@
+﻿namespace Wolflight.Utilities.Exceptions.UnitTests
+{
+    public class ArgumentZeroExceptionTests
+    {
+        public class ThrowIfZeroMethod
+        {
+            [Fact]
+            public void ThrowsWhenZeroInt()
+            {
+                ArgumentZeroException ex;
+                Action<int> target = (x => { ArgumentZeroException.ThrowIfZero(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0));
+
+                Assert.Equal("The provided argument was zero. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(int.MaxValue);
+                target(1);
+                target(-1);
+                target(int.MinValue);
+            }
+
+            [Fact]
+            public void ThrowsWhenZeroLong()
+            {
+                ArgumentZeroException ex;
+                Action<long> target = (x => { ArgumentZeroException.ThrowIfZero(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0L));
+
+                Assert.Equal("The provided argument was zero. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(long.MaxValue);
+                target(1L);
+                target(-1L);
+                target(long.MinValue);
+            }
+
+            [Fact]
+            public void ThrowsWhenZero()
+            {
+                ArgumentZeroException ex;
+                Action<decimal> target = (x => { ArgumentZeroException.ThrowIfZero(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0M));
+
+                Assert.Equal("The provided argument was zero. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(decimal.MaxValue);
+                target(1M);
+                target(-1M);
+                target(decimal.MinValue);
+            }
+        }
+
+        public class ThrowIfZeroOrLessMethod
+        {
+            [Fact]
+            public void ThrowsWhenZeroInt()
+            {
+                ArgumentZeroException ex;
+                Action<int> target = (x => { ArgumentZeroException.ThrowIfZeroOrLess(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(int.MaxValue);
+                target(1);
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(-1));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+            }
+
+            [Fact]
+            public void ThrowsWhenZeroLong()
+            {
+                ArgumentZeroException ex;
+                Action<long> target = (x => { ArgumentZeroException.ThrowIfZeroOrLess(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0L));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(long.MaxValue);
+                target(1L);
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(-1L));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+            }
+
+            [Fact]
+            public void ThrowsWhenZero()
+            {
+                ArgumentZeroException ex;
+                Action<decimal> target = (x => { ArgumentZeroException.ThrowIfZeroOrLess(x); });
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(0M));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+
+                target(decimal.MaxValue);
+                target(1M);
+
+                ex = Assert.Throws<ArgumentZeroException>(() => target(-1M));
+
+                Assert.Equal("The provided argument was zero or less. (Parameter 'x')", ex.Message);
+                Assert.Equal("x", ex.ParamName);
+            }
+        }
+
+
+    }
+}
